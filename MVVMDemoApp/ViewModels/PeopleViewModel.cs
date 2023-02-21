63422e8672581ada7db5c0f7a8d7ce8b4dfbe7df
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-02-21 15:37:58 +0200

    Create PeopleView and PeopleViewModel

---
+++ MVVMDemoApp/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using MVVMDemoApp.Models;
+using MVVMDemoApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
+
+namespace MVVMDemoApp.ViewModels
+{
+    public class PeopleViewModel
+    {
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+
+        public RelayCommand NavigateToAddPersonViewCommand
+        {
+            get
+            {
+                if (_navigateToAddPersonViewCommand is null)
+                {
+                    _navigateToAddPersonViewCommand = new RelayCommand(() =>
+                    {
+                        MainPage.Instance.ViewModel.CurrentView = AddPersonView.Instance;
+                    });
+                }
+                return _navigateToAddPersonViewCommand;
+            }
+        }
+        public RelayCommand<Person> DeletePersonCommand
+        {
+            get
+            {
+                if (_deletePersonCommand is null)
+                {
+                    _deletePersonCommand = new RelayCommand<Person>(async (person) =>
+                    {
+                        var dialog = new MessageDialog("Are you sure you want to delete this person?", "Delete Person");
+                        dialog.Commands.Add(new UICommand("Yes", new UICommandInvokedHandler((cmd) => People.Remove(person))));
+                        dialog.Commands.Add(new UICommand("No"));
+                        await dialog.ShowAsync();
+                    });
+                }
+                return _deletePersonCommand;
+            }
+        }
+
+        private RelayCommand _navigateToAddPersonViewCommand;
+        private RelayCommand<Person> _deletePersonCommand;
+        
+        public PeopleViewModel()
+        {
+            People.Add(new Person("John", "Smith"));
+        }
+    }
+}
---
+++ MVVMDemoApp/Views/PeopleView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MVVMDemoApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MVVMDemoApp.Views
+{
+
+    public sealed partial class PeopleView : UserControl
+    {
+        public PeopleViewModel ViewModel { get; set; }
+
+        private static PeopleView _instance;
+        public static PeopleView Instance
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    _instance = new PeopleView();
+                }
+                return _instance;
+            }
+            private set
+            {
+                _instance = value;
+            }
+        }
+
+        public PeopleView()
+        {
+            ViewModel = new PeopleViewModel();
+            InitializeComponent();
+            Instance = this;
+        }
+    }
+}
